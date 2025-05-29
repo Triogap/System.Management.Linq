a8963e6db375f7d3a820be98862e73281ca07f47
Triogap <triogap@outlook.com>
2025-05-29 23:00:03 +0200

    Added documentation for the public methods and non-generated classes in the System.Management.Types library

--- Types/InstanceFactory.cs
+++ Types/InstanceFactory.cs
@@ -1,14 +1,102 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-using System.Linq.Expressions;
+﻿using System.Linq.Expressions;
 using System.Reflection;
 
 namespace System.Management.Types;
 
+/// <summary>
+/// Provides a factory for creating strongly-typed instances from WMI <see cref="ManagementObject"/> data.
+/// <para>
+/// The <c>InstanceFactory</c> caches and uses reflection to construct objects of a specified type
+/// that expose a constructor accepting a <see cref="ManagementObject"/>. It supports mapping between
+/// WMI class names and .NET types, and enables efficient instantiation of objects from WMI queries.
+/// </para>
+/// </summary>
 public record struct InstanceFactory(Type TargetType, string ClassName, Func<ManagementObject, object> Constructor)
 {
     private static readonly Dictionary<Type, InstanceFactory?> _factoriesByType = [];
     private static readonly Dictionary<string, InstanceFactory?> _factoriesByClassName = [];
+
+    /// <summary>
+    /// Determines whether an instance of the specified generic type <typeparamref name="T"/> can be created from a <see cref="ManagementObject"/>.
+    /// </summary>
+    /// <typeparam name="T">The target type to check for instantiation support.</typeparam>
+    /// <returns><c>true</c> if an instance can be created; otherwise, <c>false</c>.</returns>
+    public static bool CanCreateInstanceOf<T>()
+        => CanCreateInstanceOf(typeof(T));
+
+    /// <summary>
+    /// Determines whether an instance of the specified <see cref="Type"/> can be created from a <see cref="ManagementObject"/>.
+    /// </summary>
+    /// <param name="type">The target type to check for instantiation support.</param>
+    /// <returns><c>true</c> if an instance can be created; otherwise, <c>false</c>.</returns>
+    public static bool CanCreateInstanceOf(Type type)
+        => GetFactory(type) != null;
+
+    /// <summary>
+    /// Gets the WMI class name associated with the specified generic type <typeparamref name="T"/>.
+    /// </summary>
+    /// <typeparam name="T">The type whose WMI class name is to be retrieved.</typeparam>
+    /// <returns>The WMI class name if available; otherwise, <c>null</c>.</returns>
+    public static string? GetClassNameOf<T>()
+        => GetClassNameOf(typeof(T));
+
+    /// <summary>
+    /// Gets the WMI class name associated with the specified <see cref="Type"/>.
+    /// </summary>
+    /// <param name="type">The type whose WMI class name is to be retrieved.</param>
+    /// <returns>The WMI class name if available; otherwise, <c>null</c>.</returns>
+    public static string? GetClassNameOf(Type type)
+        => GetFactory(type)?.ClassName;
+
+    /// <summary>
+    /// Creates an instance of type <typeparamref name="T"/> from the specified <see cref="ManagementObject"/>.
+    /// </summary>
+    /// <typeparam name="T">The type of object to create.</typeparam>
+    /// <param name="managementObject">The <see cref="ManagementObject"/> to use for instantiation.</param>
+    /// <returns>An instance of type <typeparamref name="T"/> created from the management object.</returns>
+    /// <exception cref="InvalidOperationException">Thrown if an instance cannot be created for the specified type and class.</exception>
+    public static T CreateInstance<T>(ManagementObject managementObject)
+    {
+        var factory = GetFactory(managementObject.ClassPath.ClassName);
+        if (factory == null)
+        {
+            if (managementObject.GetSystemProperties().Derivations is string[] derivations)
+            {
+                foreach (var className in derivations)
+                {
+                    factory = GetFactory(className);
+                    if (factory != null)
+                    {
+                        foreach (var derivation in derivations)
+                        {
+                            if (derivation == className)
+                            {
+                                break;
+                            }
+                            _factoriesByClassName[derivation] = factory;
+                        }
+                        _factoriesByClassName[className] = factory;
+                        break;
+                    }
+                }
+            }
+
+            if (factory == null)
+            {
+                throw new InvalidOperationException($"Unable to create instance of type {typeof(T)} with class {managementObject.ClassPath.ClassName}.");
+            }
+        }
+        return factory.Value.Create<T>(managementObject);
+    }
+
+    /// <summary>
+    /// Creates a sequence of instances of type <typeparamref name="T"/> from a <see cref="ManagementObjectCollection"/>.
+    /// </summary>
+    /// <typeparam name="T">The type of objects to create.</typeparam>
+    /// <param name="managementObjectCollection">The collection of <see cref="ManagementObject"/> instances.</param>
+    /// <returns>An <see cref="IEnumerable{T}"/> of created instances.</returns>
+    public static IEnumerable<T> CreateInstances<T>(ManagementObjectCollection managementObjectCollection)
+        => managementObjectCollection.Cast<ManagementObject>().Select(CreateInstance<T>);
 
     private static InstanceFactory? GetFactory(Type type)
     {
@@ -54,55 +142,6 @@ public record struct InstanceFactory(Type TargetType, string ClassName, Func<Man
         var lambda = Expression.Lambda<Func<ManagementObject, object>>(newExpression, parameters);
         return _factoriesByClassName[className] = _factoriesByType[type] = new(type, className, lambda.Compile());
     }
-
-    public static bool CanCreateInstanceOf<T>()
-        => CanCreateInstanceOf(typeof(T));
-
-    public static bool CanCreateInstanceOf(Type type)
-        => GetFactory(type) != null;
-
-    public static string? GetClassNameOf<T>()
-        => GetClassNameOf(typeof(T));
-
-    public static string? GetClassNameOf(Type type)
-        => GetFactory(type)?.ClassName;
-
-    public static T CreateInstance<T>(ManagementObject managementObject)
-    {
-        var factory = GetFactory(managementObject.ClassPath.ClassName);
-        if (factory == null)
-        {
-            if (managementObject.GetSystemProperties().Derivations is string[] derivations)
-            {
-                foreach (var className in derivations)
-                {
-                    factory = GetFactory(className);
-                    if (factory != null)
-                    {
-                        foreach (var derivation in derivations)
-                        {
-                            if (derivation == className)
-                            {
-                                break;
-                            }
-                            _factoriesByClassName[derivation] = factory;
-                        }
-                        _factoriesByClassName[className] = factory;
-                        break;
-                    }
-                }
-            }
-
-            if (factory == null)
-            {
-                throw new InvalidOperationException($"Unable to create instance of type {typeof(T)} with class {managementObject.ClassPath.ClassName}.");
-            }
-        }
-        return factory.Value.Create<T>(managementObject);
-    }
-
-    public static IEnumerable<T> CreateInstances<T>(ManagementObjectCollection managementObjectCollection)
-        => managementObjectCollection.Cast<ManagementObject>().Select(CreateInstance<T>);
 
     private readonly T Create<T>(ManagementObject managementObject)
     {
